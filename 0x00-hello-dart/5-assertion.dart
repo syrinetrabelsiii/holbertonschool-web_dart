b6c7bc9bd77eedaebbe2c4a851cb5a789786a762
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-10-31 16:50:41 +0100

    assertion

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,10 +1,8 @@
 void main(List<String> args) {
- int score = 90; // replace this with the score you want to check
+  /*
+  Write Your code below 
+   */
+  assert(int.parse(args[0]) >= 80, "The score must be bigger or equal to 80");
 
- try {
-   assert(score >= 80, 'The score must be bigger or equal to 80');
-   print('You Passed');
- } catch (e) {
-   print('Uncaught Error: $e');
- }
+  print('You Passed');
 }
