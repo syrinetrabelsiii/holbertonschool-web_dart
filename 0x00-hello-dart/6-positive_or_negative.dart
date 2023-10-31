4ff11ba2b404c1e4a944b6a6a966b77ffad78370
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-10-31 17:41:15 +0100

    pos or neg

--- 0x00-hello-dart/6-positive_or_negative.dart
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -1,12 +1,10 @@
-void main() {
- String numberAsString = '5';
- int number = int.parse(numberAsString);
-
- if (number > 0) {
-    print('The number $number is positive');
- } else if (number == 0) {
-    print('The number $number is zero');
- } else {
-    print('The number $number is negative');
- }
+void main (List<String> args) {
+  var number = int.parse(args[0]);
+  if (number > 0) {
+    print("${number} is positive");
+  } else if (number < 0) {
+    print("${number} is negative");
+  } else {
+    print("${number} is zero");
+  }
 }
