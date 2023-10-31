3f159b2c89382c388fe2ef679453ca13e73cf388
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-10-31 16:20:45 +0100

    print string

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,5 +1,5 @@
 void main() {
-  String str = "Holberton School";
-  print("$str$str$str\n");
-  print(str.substring(0. 9));
+String str = "Holberton School";
+ print("${str * 3}\n");
+ print(str.substring(0, 9) + '\n');
 }
