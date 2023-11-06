a1ca0e5a67b1bd06262540e3628d5b8b28ba7b14
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-11-06 21:50:44 +0100

    add your password

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -4,11 +4,11 @@ class Password{
     return (
       (password.length >= 8) &&
       (password.length <= 16) &&
-      (password.contains(new RegExp(r'[A-Z]'))) &&
-      (password.contains(new RegExp(r'[a-z]'))) &&
-      (password.contains(new RegExp(r'[0-9]')));
+      (password.contains( RegExp(r'[A-Z]'))) &&
+      (password.contains( RegExp(r'[a-z]'))) &&
+      (password.contains( RegExp(r'[0-9]')));
   }
-  @Override
+  @override
   String toString(){
   return 'Your Password is: ${this.password}';
 }
